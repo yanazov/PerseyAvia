3556d956559625307ff0d98af043f68a9924d968
yanazov <yan_azov@mail.ru>
2013-11-20 11:25:24 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -22,12 +22,17 @@ var calendar_left = -293;
 var tab1_check_status = 0;
 var tab1_people_open = 0;
 var tab1_detail_open = 0;
+var tab1_find_open = 0;
+var tab2_people_open = 0;
+var tab2_detail_open = 0;
+var tab2_find_open = 0;
 var start_bar_x = -1;
 var select_bar = -1;
 var calendar_move = 0;
 var left_menu_tab1 = 1;
 var left_menu_tab2 = 0;
 var end_city = 0;
+var current_tab = 1;
 
 window.onload = function() {	
 	
@@ -84,16 +89,20 @@ function js_set_size(){
 	document.getElementsByClassName("center_fon")[0].style.height = document.body.clientHeight - 104 + "px";
 	document.getElementsByClassName("center_fon")[0].style.width = document.body.clientWidth - 371 + "px";
 	document.getElementsByClassName("order_tab1")[0].style.height = document.body.clientHeight - 155 + "px";
-	document.getElementsByClassName("order_tab1_main")[0].style.height = document.body.clientHeight - 155 + "px";
+	document.getElementsByClassName("order_tab1_main")[0].style.height = document.body.clientHeight - 155 - 70 + "px";
+	document.getElementsByClassName("order_tab2_main")[0].style.height = document.body.clientHeight - 155 - 70 + "px";
+	document.getElementsByClassName("order_tab1_back")[0].style.height = document.body.clientHeight - 155 - 70 + "px";
+	document.getElementsByClassName("order_tab2_back")[0].style.height = document.body.clientHeight - 155 - 70 + "px";
 	document.getElementsByClassName("order_tab2")[0].style.height = document.body.clientHeight - 155 + "px";
 	document.getElementsByClassName("calendar_block")[0].style.height = document.body.clientHeight - 60 + "px";
 	document.getElementsByClassName("calendar_cont")[0].style.height = document.body.clientHeight - 130 + "px";
 	jQuery('.order_tab1_main').jScrollPane();
+	jQuery('.order_tab2_main').jScrollPane();
 	/*jQuery('.order_tab1').jScrollPane();*/
 }
 
 function js_main_slider(){
-	/*delay_slide -= 3;*/
+	delay_slide -= 3;
 	if(delay_slide <= 0){
 		tmp_time += 1;
 		if(i_slaider < 100){
@@ -119,8 +128,8 @@ function js_main_slider(){
 /* ===========================================  BEGIN LEFT MENU ====================================================== */	
 	if(menu_tab == 1){
 		select_tab_x -= 23;
-		left_menu_tab1 +=0.05;
-		left_menu_tab2 -=0.05;
+		left_menu_tab1 +=0.09;
+		left_menu_tab2 -=0.09;
 		document.getElementsByClassName("order_tab1")[0].style.opacity = left_menu_tab1;
 		document.getElementsByClassName("order_tab2")[0].style.opacity = left_menu_tab2;	
 		if(select_tab_x < 0 && left_menu_tab2 < 0){
@@ -133,12 +142,17 @@ function js_main_slider(){
 		if(select_tab_x < 0){
 			select_tab_x = 0;
 		}
+		if(left_menu_tab1 > 1){
+			left_menu_tab1 = 1;
+			left_menu_tab2 = 0;
+		}
 		document.getElementsByClassName("select_tab")[0].style.left = select_tab_x + "px";
 	}
 	if(menu_tab == 2){
+		jQuery('.order_tab2_main').jScrollPane();
 		select_tab_x += 23;		
-		left_menu_tab1 -=0.05;
-		left_menu_tab2 +=0.05;
+		left_menu_tab1 -=0.09;
+		left_menu_tab2 +=0.09;
 		document.getElementsByClassName("order_tab1")[0].style.opacity = left_menu_tab1;
 		document.getElementsByClassName("order_tab2")[0].style.opacity = left_menu_tab2;
 		document.getElementsByClassName("order_tab2")[0].style.display = "block";
@@ -150,7 +164,12 @@ function js_main_slider(){
 			menu_tab = 0;
 		}
 		if(select_tab_x > 140){
+			jQuery('.order_tab2_main').jScrollPane();
 			select_tab_x = 140;
+		}
+		if(left_menu_tab1 < 0){
+			left_menu_tab1 = 0;
+			left_menu_tab2 = 1;
 		}
 		document.getElementsByClassName("select_tab")[0].style.left = select_tab_x + "px";
 	}
@@ -178,16 +197,20 @@ function js_main_slider(){
 /* =============================================== END RIGNT SLIDER ================================================== */ 
 /* =============================================== BEGIN LOGIN FORM ============================================ */
 	if(login_open == 1 && y_login_form < 60){
-		y_login_form += 3;
+		y_login_form += 5;
 		if(y_login_form >= 60){
 			y_login_form = 60
 			login_open = 0;
+			document.getElementsByClassName("login_form_fon")[0].style.zIndex = 600;
 		}
 		document.getElementsByClassName("login_form")[0].style.top = y_login_form + "px";
+		
+		
 	
 	}
 	if(login_close == 1){
-		y_login_form -= 3;
+		document.getElementsByClassName("login_form_fon")[0].style.zIndex = 500;
+		y_login_form -= 5;
 		document.getElementsByClassName("login_form")[0].style.top = y_login_form + "px";
 		if(y_login_form < -145){
 			login_close = 0;
@@ -212,11 +235,38 @@ function js_main_slider(){
 		if(tab1_people_open == 1)
 			tab1_people_open = 10;
 	}
+	if(tab2_people_open > 0 && tab2_people_open < 1.1){
+		tab2_people_open += 0.02
+			
+		document.getElementsByClassName("tab2_people")[0].style.opacity = tab2_people_open;
+		if(tab2_people_open == 1)
+			tab2_people_open = 10;
+	}
 	if(tab1_detail_open > 0 && tab1_detail_open < 1.1){
 		tab1_detail_open += 0.02
-		document.getElementsByClassName("tab1_detail")[0].style.opacity = tab1_detail_open;
+		document.getElementsByClassName("tab1_detail")[0].style.opacity = tab1_detail_open;		
 		if(tab1_detail_open == 1)
 			tab1_detail_open = 10;
+	}
+	if(tab2_detail_open > 0 && tab2_detail_open < 1.1){
+		tab2_detail_open += 0.02
+		document.getElementsByClassName("tab2_detail")[0].style.opacity = tab2_detail_open;		
+		if(tab2_detail_open == 1)
+			tab2_detail_open = 10;
+	}
+	if(tab1_find_open > 0 && tab1_find_open < 1.1){
+		tab1_find_open += 0.02
+		document.getElementsByClassName("tab1_find")[0].style.opacity = tab1_find_open;
+		
+		if(tab1_find_open == 1)
+			tab1_find_open = 10;
+	}
+	if(tab2_find_open > 0 && tab2_find_open < 1.1){
+		tab2_find_open += 0.02
+		document.getElementsByClassName("tab2_find")[0].style.opacity = tab2_find_open;
+		
+		if(tab2_find_open == 1)
+			tab2_find_open = 10;
 	}
 	
 /* =============================================== END VIEW LEFT BLOCKS ============================================ */		
@@ -296,7 +346,7 @@ function js_menu_tab(tab){
 			clearInterval(menu_tab_timer);
 		}
 	}
-	
+	current_tab = tab;
 }
 
 function js_menu_tab1(){
@@ -400,6 +450,15 @@ function js_number_up(fild_name){
 	js_next_detail(fild_name)
 }
 
+function js_number_up2(fild_name){
+	var tmp = parseInt(document.getElementsByClassName(fild_name)[0].value);
+	if(document.getElementsByClassName(fild_name)[0].value == "")
+		tmp = 0;
+	tmp += 1;
+	document.getElementsByClassName(fild_name)[0].value = tmp;
+	js_next_detail2(fild_name);
+}
+
 function js_number_dwn(fild_name){
 	var tmp = parseInt(document.getElementsByClassName(fild_name)[0].value);
 	tmp -= 1;
@@ -408,13 +467,33 @@ function js_number_dwn(fild_name){
 	js_next_detail(fild_name)
 }
 
+function js_number_dwn2(fild_name){
+	var tmp = parseInt(document.getElementsByClassName(fild_name)[0].value);
+	tmp -= 1;
+	if (tmp > -1)
+		document.getElementsByClassName(fild_name)[0].value = tmp;
+	js_next_detail2(fild_name);
+}
+
 function js_next_detail(fild_name){
 	if(document.getElementsByClassName(fild_name)[0].value != ""){
-		tab1_detail_open = 0.2;
+		if(tab1_detail_open == 0)
+			tab1_detail_open = 0.2;
 		document.getElementsByClassName("tab1_detail")[0].style.display = "block";
 		document.getElementsByClassName("tab1_detail")[1].style.opacity = 0;
 		
 		jQuery('.order_tab1_main').jScrollPane();
+	}
+}
+
+function js_next_detail2(fild_name){
+	if(document.getElementsByClassName(fild_name)[0].value != ""){
+		if(tab2_detail_open == 0)
+			tab2_detail_open = 0.2;
+		document.getElementsByClassName("tab2_detail")[0].style.display = "block";
+		document.getElementsByClassName("tab2_detail")[1].style.opacity = 0;
+		
+		jQuery('.order_tab2_main').jScrollPane();
 	}
 }
 
@@ -449,14 +528,47 @@ function js_select_form(t, number){
 		jQuery('.tab1_class_select_ul').jScrollPane();
 	}
 	/*document.getElementsByClassName("tab1_class_select_ul")[number].style.display = "block"*/
-	
+}
+
+function js_select_form2(t, number){
+	document.getElementsByClassName("tab2_class_select_ul")[number].style.display = "block"
+	if(number == 1){	
+		document.getElementsByClassName("tab2_class_select_ul")[number].style.display = "block"
+		var br = t.getBoundingClientRect();
+		document.getElementsByClassName("tab2_class_select_ul")[number].style.top = br.top -135 + "px";
+		document.getElementsByClassName("tab2_class_select_ul")[number].style.left = 22 + "px";
+		
+		document.getElementsByClassName("select_form")[0].style.display = "block"
+		jQuery('.tab2_class_select_ul').jScrollPane();
+	}
+	/*document.getElementsByClassName("tab1_class_select_ul")[number].style.display = "block"*/
 }
 
 function js_select_form_li(t, number){
-	document.getElementsByClassName("tab1_class_select_fild")[number].value = t.innerHTML;
-	document.getElementsByClassName("tab1_class_select_ul")[number].style.display = "none"
-	if(number == 1){
-		document.getElementsByClassName("select_form")[0].style.display = "none"
+	if(current_tab == 1){                         /*  выбран первый таб  */
+		document.getElementsByClassName("tab1_class_select_fild")[number].value = t.innerHTML;
+		document.getElementsByClassName("tab1_class_select_ul")[number].style.display = "none"
+		
+		if(number == 1){
+			document.getElementsByClassName("select_form")[0].style.display = "none"
+		}
+	}
+	if(current_tab ==2){						/*  выбран второй таб  */
+		document.getElementsByClassName("tab2_class_select_fild")[number].value = t.innerHTML;
+		document.getElementsByClassName("tab2_class_select_ul")[number].style.display = "none"
+		
+		if(number == 1){
+			document.getElementsByClassName("select_form")[0].style.display = "none"
+		}
+	
+	}
+
+	
+	if(document.getElementsByClassName("tab1_class_select_fild")[0].value != "" && document.getElementsByClassName("tab1_class_select_fild")[1].value != ""){		
+		tab1_find_open = 0.2;
+	}
+	if(document.getElementsByClassName("tab2_class_select_fild")[0].value != "" && document.getElementsByClassName("tab2_class_select_fild")[1].value != ""){		
+		tab2_find_open = 0.2;
 	}
 }
   
@@ -483,4 +595,10 @@ function js_tab2_continue(){
 								'</span>';
 	document.getElementsByClassName("tab2_to_fild")[0].value = "";
 	document.getElementsByClassName("tab2_route")[1].style.height = document.getElementsByClassName("tab2_route")[0].offsetHeight - 11 + "px"; 
+}
+
+function js_tab2_people_open(){
+	document.getElementsByClassName("tab2_people")[0].style.display = "block";	
+	document.getElementsByClassName("tab2_people")[1].style.opacity = 0;
+	tab2_people_open = 0.2;
 }
